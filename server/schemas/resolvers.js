21aec061ece79d0568b04145ca11b333501aff03
JPB <jbanas9124@gmail.com>
2021-07-03 14:43:39 -0500

    authentication apollo server setup, typedefs, and resolvers setup

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
